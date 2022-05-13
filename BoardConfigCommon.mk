8ebd62ab195c9be39aa0041953a35e2084bfbe3f
anupam41 <anupamsamanta41@gmail.com>
2022-05-13 13:55:02 +0530

    sm6150-common: sepolicy: fix Address telephony denials

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,7 +158,7 @@ VENDOR_SECURITY_PATCH := 2022-05-01
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
